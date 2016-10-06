ce45f8af996d20ffa9d27224ff20fc8d7c9675c0
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-06 11:42:16 -0600

    Updated number_of_seconds.rb
    * Made some minor tweeks and changes to formating to satisfy
      rubocop
    * Recorrected math on seconds per year to match my original
      formula. Added the .to_i inline to convert the result to int to
      pass travis ci test.
    * Fixed bad grammar
    * All tests pass
    
    [#131780197]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -20,16 +20,17 @@ days_per_week = 7
 seconds_per_hour = seconds_per_minute * minutes_per_hour
 seconds_per_day = seconds_per_hour * hours_per_day
 seconds_per_week = seconds_per_day * days_per_week
-seconds_per_year = seconds_per_week * 52
+seconds_per_year = seconds_per_day * 365.25
 
 puts "There are #{seconds_per_minute} seconds in a minute"
 puts "There are #{minutes_per_hour} minutes in an hour"
-puts "There are #{hours_per_day} hours in an day" #should be a day, bad grammar
+puts "There are #{hours_per_day} hours in a day" # should be a day, bad grammar
 puts "There are #{days_per_week} days in a week"
 puts 'That means there are:'
 puts "  #{seconds_per_hour} seconds in an hour,"
 puts "  #{seconds_per_day} seconds in a day,"
 puts "  #{seconds_per_week} seconds in a week"
-puts "That means when you turn 20, you've been alive for #{seconds_per_year * 20} seconds,"
-puts "and if you make it to 100, you will have lived #{seconds_per_year * 100} seconds.
-Make them count!"
+puts "That means when you turn 20, you've been alive for #{(seconds_per_year *
+  20).to_i} seconds,"
+puts "and if you make it to 100, you will have lived #{(seconds_per_year *
+  100).to_i} seconds. Make them count!"
