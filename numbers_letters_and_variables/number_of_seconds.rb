3669380049ed16a1d3324880d3954ee61dbfc4b9
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-06 11:15:12 -0600

    Updated number_of_seconds
    * I updated the math to match the result expected. It follows the
      pattern to get seconds per year of seconds per week * 52 weeks,
      and does not account for the proper numbe of days in a year.
    
    * I also updated the lines to include the bad grammar of 'an day'
      and adjusting the spacing on line 33 to match ignoring rubocops
      suggestion of it being too long.
    
    [#131780197]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -10,7 +10,8 @@
 #   86400 seconds in an day,
 #   604800 seconds in a week
 # That means when you turn 20, you've been alive for 628992000 seconds,
-# and if you make it to 100, you will have lived 3144960000 seconds. Make them count!
+# and if you make it to 100, you will have lived 3144960000 seconds.
+# Make them count!
 
 seconds_per_minute = 60
 minutes_per_hour = 60
@@ -19,15 +20,16 @@ days_per_week = 7
 seconds_per_hour = seconds_per_minute * minutes_per_hour
 seconds_per_day = seconds_per_hour * hours_per_day
 seconds_per_week = seconds_per_day * days_per_week
-seconds_per_year = seconds_per_day * 365.25
+seconds_per_year = seconds_per_week * 52
 
 puts "There are #{seconds_per_minute} seconds in a minute"
 puts "There are #{minutes_per_hour} minutes in an hour"
-puts "There are #{hours_per_day} hours in a day"
+puts "There are #{hours_per_day} hours in an day" #should be a day, bad grammar
 puts "There are #{days_per_week} days in a week"
-puts "That means there are:"
+puts 'That means there are:'
 puts "  #{seconds_per_hour} seconds in an hour,"
 puts "  #{seconds_per_day} seconds in a day,"
 puts "  #{seconds_per_week} seconds in a week"
 puts "That means when you turn 20, you've been alive for #{seconds_per_year * 20} seconds,"
-puts "and if you make it to 100, you will have lived #{seconds_per_year * 100} seconds. Make them count!"
+puts "and if you make it to 100, you will have lived #{seconds_per_year * 100} seconds.
+Make them count!"
