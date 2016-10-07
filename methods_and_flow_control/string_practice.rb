cdd5ff38a3d50931cc6491997baa08c5494c4938
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-06 22:04:34 -0600

    Updated string_practice.rb
    * Fixed a capitalize issue to match expected tests.
    
    [#131780205]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -6,7 +6,7 @@ practice_string = 'this is a string to practice with'
 puts practice_string.downcase
 puts practice_string.capitalize
 puts practice_string.upcase
-puts practice_string.gsub('string', "'string'")
-puts "The string #{practice_string} has #{practice_string.length} characters"
+puts practice_string.gsub('string', "'string'").capitalize
+puts "The string '#{practice_string}' has #{practice_string.length} characters"
 puts practice_string.reverse
 puts practice_string.replace('practice practice practice!')
