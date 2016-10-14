b6b4825c7e72c435e4c7ad6c0cbf23972fc4cb2c
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-13 18:08:30 -0600

    Updated mad_libs.rb
    * Updated mad_libs to please rubocop.
    * Fixed all rubocop errors, except that show_mad_lib method
      is calling too many array.pop. The code runs, but rubocop
      is angry.
    
    [#131780213]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -6,13 +6,11 @@
 class MadLibs
   def initialize
     @given_words = []
-    word = @word
   end
 
-  def word_types(wtype)
-    puts "Please enter a(n) #{wtype}."
-    @word = gets.chomp
-    @given_words.push(@word)
+  def ask_for_a(word_type)
+    puts "Please enter a(n) #{word_type}."
+    @given_words.push(gets.chomp)
   end
 
   def array_reverse
@@ -20,44 +18,40 @@ class MadLibs
   end
 
   def show_mad_lib
-    puts "A vacation is when you take a trip to some #{@given_words.pop} place "
-    puts "with your #{@given_words.pop} family. Usually you go to some place"
-    puts "that is near a/an #{@given_words.pop} or up on a/an #{@given_words.pop}."
-    puts "A good vacation place is one where you can ride #{@given_words.pop} "
-    puts "or play #{@given_words.pop} or go hunting for #{@given_words.pop}. I like"
-    puts "to spend my time #{@given_words.pop} or #{@given_words.pop}."
-    puts 'When parents go on a vacation, they spend their time eating '
-    puts "three #{@given_words.pop} a day, and fathers play golf, and mothers "
-    puts "sit around #{@given_words.pop}. Last summer, my little brother "
-    puts "fell in a/an #{@given_words.pop} and got poison #{@given_words.pop} all "
-    puts "over his #{@given_words.pop}. My family is going to go to (the) "
-    puts "#{@given_words.pop}, and I will practice #{@given_words.pop}. Parents "
-    puts 'need vacations more than kids because parents are always very '
-    puts "#{@given_words.pop} and because they have to work #{@given_words.pop} "
-    puts "hours every day all year making enough #{@given_words.pop} to pay "
-    puts 'for the vacation.'
+    puts <<-END_OF_MADLIB
+    A vacation is when you take a trip to some #{@given_words.pop} place with your #{@given_words.pop} family.
+    Usually you go to some place that is near a/an #{@given_words.pop} or up on a/an #{@given_words.pop}.
+    A good vacation place is one where you can ride #{@given_words.pop} or play #{@given_words.pop} or go hunting
+    for #{@given_words.pop}. I like to spend my time #{@given_words.pop} or #{@given_words.pop}. When parents go
+    on a vacation, they spend their time eating three #{@given_words.pop} a day, and fathers play golf, and mothers
+    sit around #{@given_words.pop}. Last summer, my little brother fell in a/an #{@given_words.pop} and got poison
+    #{@given_words.pop} all over his #{@given_words.pop}. My family is going to go to (the) #{@given_words.pop},
+    and I will practice #{@given_words.pop}. Parents need vacations more than kids because parents are always very
+    #{@given_words.pop} and because they have to work #{@given_words.pop} hours every day all year making enough
+    #{@given_words.pop} to pay for the vacation.
+END_OF_MADLIB
   end
 end
 
 ml = MadLibs.new
-ml.word_types('adjective')
-ml.word_types('adjective')
-ml.word_types('noun')
-ml.word_types('noun')
-ml.word_types('plural noun')
-ml.word_types('game')
-ml.word_types('plural noun')
-ml.word_types('verb ending in "ing"')
-ml.word_types('verb ending in "ing"')
-ml.word_types('plural noun')
-ml.word_types('verb ending in "ing"')
-ml.word_types('noun')
-ml.word_types('plant')
-ml.word_types('part of the body')
-ml.word_types('place')
-ml.word_types('verb ending in "ing"')
-ml.word_types('adjective')
-ml.word_types('number')
-ml.word_types('plural noun')
+ml.ask_for_a('adjective')
+ml.ask_for_a('adjective')
+ml.ask_for_a('noun')
+ml.ask_for_a('noun')
+ml.ask_for_a('plural noun')
+ml.ask_for_a('game')
+ml.ask_for_a('plural noun')
+ml.ask_for_a('verb ending in "ing"')
+ml.ask_for_a('verb ending in "ing"')
+ml.ask_for_a('plural noun')
+ml.ask_for_a('verb ending in "ing"')
+ml.ask_for_a('noun')
+ml.ask_for_a('plant')
+ml.ask_for_a('part of the body')
+ml.ask_for_a('place')
+ml.ask_for_a('verb ending in "ing"')
+ml.ask_for_a('adjective')
+ml.ask_for_a('number')
+ml.ask_for_a('plural noun')
 ml.array_reverse
 ml.show_mad_lib
