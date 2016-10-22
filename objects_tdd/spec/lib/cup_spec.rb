013b7da5c3067bece521930149d3571ae5238cc0
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-22 11:58:24 -0600

    Updated cup.rb and cup_spec.rb
    * I added failing tests to cup_spec.rb, added code to cup.rb to
      make tests pass, repeated till Cup class was completely built.
    
    [#132363129]

--- objects_tdd/spec/lib/cup_spec.rb
+++ objects_tdd/spec/lib/cup_spec.rb
@@ -1,2 +1,27 @@
 require './lib/cup'
 require 'rspec'
+
+describe Cup do
+  let(:cup) { Cup.new }
+  describe '.cup_size(size)' do
+    it 'returns the cup size chosen, adult by default' do
+      expect(cup.size('adult')).to eq('adult')
+    end
+  end
+  describe '.cup_color(color)' do
+    it 'returns the cup color chosen, clear by default' do
+      expect(cup.cup_color('clear')).to eq('clear')
+    end
+  end
+  describe '.beverage_choice(flavor)' do
+    it 'returns the flavor of drink chosen, water by default' do
+      expect(cup.beverage_choice('water')).to eq('water')
+    end
+  end
+  describe '.cup_volume(volume)' do
+    it 'returns the amount of beverage the user chooses.' do
+      expect(cup.cup_volume('full')).to eq('full')
+      expect(cup.cup_volume('half')).to eq('half')
+    end
+  end
+end
