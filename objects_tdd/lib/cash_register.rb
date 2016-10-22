99d8fabca1359bff27fa72081733455a1ed8ed9a
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-21 20:11:36 -0600

    Updated objects_tdd cash_register
    * Added cash_register_spec.rb for testing. Added test, updated
      code in cash_register.rb to pass test, added new failing test.
      Lather, rinse, repeat.
    * Got all test created to pass and meet conditions.
    
    [#132363097]

--- objects_tdd/lib/cash_register.rb
+++ objects_tdd/lib/cash_register.rb
@@ -1,2 +1,27 @@
+#!/usr/bin/env ruby
+
+# This is a TDD CashRegister
 class CashRegister
+  attr_reader :total
+
+  def initialize
+    @total = 0.00
+  end
+
+  def purchase(amount)
+    @total += amount
+  end
+
+  def round_decimal(number)
+    format('%.2f', number)
+  end
+
+  def pay(amount)
+    if amount <= @total
+      @total -= amount
+    else
+      # @total -= amount
+      "Your change due is #{round_decimal((@total -= amount).to_f.abs)}."
+    end
+  end
 end
--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -1,9 +1,29 @@
 require './lib/cash_register'
+require 'rspec'
 
 describe CashRegister do
-  describe '.total'  do
+  let(:register) { CashRegister.new }
+  describe '.total' do
     it 'returns the total of the amount owed' do
-      expect(CashRegister.total?).to eq(0.00)
+      expect(register.total).to eq(0.00)
+    end
+  end
+  describe '.puchase(amount)' do
+    it 'adds the amount passed to the balance and balance should return the
+  amount of total' do
+      expect(register.purchase(3.00)).to eq(3.00)
+    end
+  end
+  describe '.pay(amount)' do
+    it 'takes an amount and subtracts it from the total, it returns either how
+  much is owed if amount payed is < amount owed' do
+      register.purchase(3.00)
+      expect(register.pay(2.00)).to eq(1.00)
+    end
+    it 'returns change due if amount payed is > amount owed' do
+      register.purchase(3.00)
+      expect(register.pay(4.00)).to eq('Your change due is 1.00.')
+      expect(register.total)
     end
   end
 end
