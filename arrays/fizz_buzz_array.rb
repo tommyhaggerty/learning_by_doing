1f525c389cd5ec73565d3b87994a957c21330c48
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-21 20:49:29 -0600

    Updated fizz_buzz_array.rb
    * Changed .map! to .map and stored it into fizz_buzz_array
    * Updated code to use if elsif for tests
    
    [#131780225]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 # Tommy Haggerty - FizzBuzz
-@count_array = (1..100).to_a
+count_array = (1..100).to_a
 
-@count_array.map! { |x| (x % 15).zero? ? 'FizzBuzz' : x }
-@count_array.map! { |x| (x % 5)== 0 ? 'Buzz' : x }
-@count_array.map! { |x| x % 3 == 0 ? 'Fizz' : x }
-puts @count_array
+fizz_buzz_array = count_array.map do |number|
+  if (number % 15).zero?
+    'FizzBuzz'
+  elsif (number % 5).zero?
+    'Buzz'
+  elsif (number % 3).zero?
+    'Fizz'
+  else number
+  end
+end
+puts fizz_buzz_array
