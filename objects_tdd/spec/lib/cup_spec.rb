bd0501494e896b829fb6227f60565e6b839489a7
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-11-13 13:35:00 -0700

    Updated cup_spec.rb
    * I updated the describe lines with # rather than .
    * I updated each test to test the defaulted values, rather than
      a passed in value.
    
    [#132363129]

--- objects_tdd/spec/lib/cup_spec.rb
+++ objects_tdd/spec/lib/cup_spec.rb
@@ -3,24 +3,24 @@ require 'rspec'
 
 describe Cup do
   let(:cup) { Cup.new }
-  describe '.cup_size(size)' do
+  describe '#cup_size(size)' do
     it 'returns the cup size chosen, adult by default' do
-      expect(cup.size('adult')).to eq('adult')
+      expect(cup.size(size)).to eq('adult')
     end
   end
-  describe '.cup_color(color)' do
+  describe '#cup_color(color)' do
     it 'returns the cup color chosen, clear by default' do
-      expect(cup.cup_color('clear')).to eq('clear')
+      expect(cup.cup_color(color)).to eq('clear')
     end
   end
-  describe '.beverage_choice(flavor)' do
+  describe '#beverage_choice(flavor)' do
     it 'returns the flavor of drink chosen, water by default' do
-      expect(cup.beverage_choice('water')).to eq('water')
+      expect(cup.beverage_choice(flavor)).to eq('water')
     end
   end
-  describe '.cup_volume(volume)' do
+  describe '#cup_volume(volume)' do
     it 'returns the amount of beverage the user chooses.' do
-      expect(cup.cup_volume('full')).to eq('full')
+      expect(cup.cup_volume(volume)).to eq('full')
       expect(cup.cup_volume('half')).to eq('half')
     end
   end
